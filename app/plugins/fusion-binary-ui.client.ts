0ac6986e65d408806a49032bd8341f63d30e341b
Romeo <romep@pageit.io>
2026-01-02 18:51:59 +0000

    Update

--- app/plugins/fusion-binary-ui.client.ts
+++ app/plugins/fusion-binary-ui.client.ts
@@ -1,7 +1,7 @@
-import { defineNuxtPlugin } from '#imports'
-import FusionBinaryUI from 'fusion-binary-ui'
-import 'fusion-binary-ui/fusion-binary-ui.css'
+// import { defineNuxtPlugin } from '#imports'
+// import FusionBinaryUI from 'fusion-binary-ui'
+// import 'fusion-binary-ui/fusion-binary-ui.css'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(FusionBinaryUI)
-})
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(FusionBinaryUI)
+// })
